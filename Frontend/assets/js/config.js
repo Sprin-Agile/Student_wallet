a323240738e7e57818af0439e0fcaaee5361156d
trong123432 <5001104170@student.hcmue.edu.vn>
2026-04-12 21:39:56 +0700

    c0

--- Frontend/assets/js/config.js
+++ Frontend/assets/js/config.js
@@ -1,5 +1,5 @@
 const API_URL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
     ? "http://127.0.0.1:5000"
-    : "https://student-wallet-z6oe.onrender.com";
+    : "https://student-wallet-z6ce.onrender.com";
 
 console.log("Đang kết nối tới API:", API_URL);
